c9a342e99a0367d9532c974f95e1f256bae98a85
pospelovpavela-design <pospelovpavel.a@gmail.com>
2026-03-07 20:34:28 +0300

    Initial Commit

---
+++ TriathlonCoach/TriathlonCoachApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriathlonCoachApp.swift
+//  TriathlonCoach
+//
+//  Created by Павел Поспелов on 07.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct TriathlonCoachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
